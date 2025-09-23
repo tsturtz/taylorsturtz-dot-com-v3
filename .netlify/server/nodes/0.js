aba53c66c2a84a1f15384b510fa861bb45d6246d
Taylor Sturtz <taylorsturtz@gmail.com>
2025-09-22 22:44:12 -0700

    dep stuff

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const file = '_app/immutable/components/layout.svelte-938c07d3.js';
-export const imports = ["_app/immutable/components/layout.svelte-938c07d3.js","_app/immutable/chunks/index-fa621b33.js"];
-export const stylesheets = [];
-export const fonts = [];
